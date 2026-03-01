995ef120b01604f19058c386ac980c6a0b186a77
ERRORniku404 <nikuu4879@gmail.com>
2026-03-01 16:19:03 +0400

    fix nested repo

---
+++ src/components/CustomText.jsx
@@ -0,0 +1,11 @@
+function CustomText({ text }) {
+  return (
+    <div>
+      <h1 className="text-[40px] text-text-main leading-[1.05] sm:text-[56px] md:text-[64px]">
+        {text}
+      </h1>
+    </div>
+  );
+}
+
+export default CustomText;
